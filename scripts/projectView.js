a78468125d7470610284f9274cccdd454091373c
ercain03 <ercain3@gmail.com>
2016-05-19 22:22:56 -0700

    still broken but getting there

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,19 +1,28 @@
-var projectsView = {};
+(function(module) {
+  var projectsView = {};
 
-projectsView.handleMainNav = function() {
-  $('.main-nav').on('click', '.nav-li', function(e) {
-    e.preventDefault();
-    $('.page-content').hide();
-    var $clickEvent = $(this);
-    $('[id="' + $clickEvent.attr('data-content') + '"]').show();
-    $('.top-menu').removeClass('expand');
-  });
-  $('.main-nav .nav-li:first').click();
-};
+  projectsView.handleMainNav = function() {
+    $('.main-nav').on('click', '.nav-li', function(e) {
+      e.preventDefault();
+      $('.page-content').hide();
+      var $clickEvent = $(this);
+      $('[id="' + $clickEvent.attr('data-content') + '"]').show();
+      $('.top-menu').removeClass('expand');
+    });
+    $('.main-nav .nav-li:first').click();
+  };
 
-$(document).ready(function() {
-  projectsView.handleMainNav();
-  $('.nav-li').click(function() {
-    $('.top-menu').toggleClass('expand');
+  projectsView.initProjectContent = function() {
+    Project.all.forEach(function (data) {
+      $('#articles').append(data.toHtml($('#article-template')));
+    });
+  };
+
+  $(document).ready(function() {
+    projectsView.handleMainNav();
+    $('.nav-li').click(function() {
+      $('.top-menu').toggleClass('expand');
+    });
   });
-});
+  module.projectsView = projectsView;
+})(window);
